8b0ebc1cf12898a80fbe920fb3c193ed9672733f
jponcecl <juan.ponce.contreras@gmail.com>
2023-09-12 16:51:31 -0300

    Reutilizacion de base html

--- AppE3/urls.py
+++ AppE3/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', inicio),
-    path('producto/', producto),
-    path('categoria/', categoria),
-    path('codigobarra/', codigobarra),
-    path('usuario/', usuario),
+    path('', inicio, name="inicio"),
+    path('producto/', producto, name="producto"),
+    path('categoria/', categoria, name="categoria"),
+    path('codigobarra/', codigobarra, name="codigobarra"),
+    path('usuario/', usuario, name="usuario"),
 ]
